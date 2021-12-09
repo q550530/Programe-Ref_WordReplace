ee148426131878bee066731748964ad57e723de1
q550530 <93378241+q550530@users.noreply.github.com>
2021-12-09 14:00:22 +0800

    Initial commit

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Find = New System.Windows.Forms.Label()
+        Me.Replace = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout
+        '
+        'Find
+        '
+        Me.Find.AutoSize = true
+        Me.Find.Location = New System.Drawing.Point(232, 43)
+        Me.Find.Name = "Find"
+        Me.Find.Size = New System.Drawing.Size(26, 12)
+        Me.Find.TabIndex = 0
+        Me.Find.Text = "Find"
+        '
+        'Replace
+        '
+        Me.Replace.AutoSize = true
+        Me.Replace.Location = New System.Drawing.Point(232, 99)
+        Me.Replace.Name = "Replace"
+        Me.Replace.Size = New System.Drawing.Size(42, 12)
+        Me.Replace.TabIndex = 1
+        Me.Replace.Text = "Replace"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(28, 33)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 57)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Replace"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(299, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(137, 22)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(299, 89)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(137, 22)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(20, 150)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Folder path"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(84, 147)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(352, 22)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(84, 191)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 12)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "123"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 12!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(509, 306)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Replace)
+        Me.Controls.Add(Me.Find)
+        Me.Name = "Form1"
+        Me.Text = "Word Replace"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents Find As System.Windows.Forms.Label
+    Friend WithEvents Replace As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Data.OleDb.OleDbConnection
+Imports System.Data.OleDb
+Imports Word = Microsoft.Office.Interop.Word
+
+
+Public Class Form1
+
+    
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim documentFormat As Object = 8
+        Dim DirInfo As IO.DirectoryInfo
+        Dim WithoutExe, outputFilename As String
+        DirInfo = New IO.DirectoryInfo(TextBox3.Text)
+        For Each file In DirInfo.GetFiles("*.doc", IO.SearchOption.TopDirectoryOnly) 'Find only for docx file
+
+
+
+            WithoutExe = Path.GetFileNameWithoutExtension(file.Name)
+            
+            Dim objApp As New Word.Application
+            objApp.Visible = True
+
+            'Open an existing document.  
+            Dim objDoc As Word.Document = objApp.Documents.Open(TextBox3.Text & "\" & file.Name)
+            objDoc = objApp.ActiveDocument
+
+            'Find and replace some text  
+            objDoc.Content.Find.Execute(FindText:=TextBox1.Text, ReplaceWith:=TextBox2.Text, Replace:=Word.WdReplace.wdReplaceAll)
+            While objDoc.Content.Find.Execute(FindText:="  ", Wrap:=Word.WdFindWrap.wdFindContinue)
+                objDoc.Content.Find.Execute(FindText:="  ", ReplaceWith:=" ", Replace:=Word.WdReplace.wdReplaceAll, Wrap:=Word.WdFindWrap.wdFindContinue)
+            End While
+
+            'outputFilename = System.IO.Path.ChangeExtension(file.Name, "htm")
+            ''Close
+            objDoc.SaveAs(TextBox3.Text & "\" & WithoutExe, documentFormat)
+            
+            objDoc.Close()
+            objApp.Quit()
+            objDoc = Nothing
+            objApp = Nothing
+
+        Next file
+
+
+
+    End Sub
+
+   
+End Class
